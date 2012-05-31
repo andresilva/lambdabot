1ff7919c11eb918b9f4ebc2ce7ef577f97a6ffda
Andre Silva <andre.beat@gmail.com>
2012-05-31 03:32:45 +0100

    Removed test stuff from build.

--- project/Build.scala
+++ project/Build.scala
@@ -23,27 +23,16 @@ object Build extends Build {
       javaOptions += "-Xmx1g",
 
       libraryDependencies ++= Seq(
-        "org.specs2" %% "specs2" % "1.8.2" % "test",
-        "org.pegdown" % "pegdown" % "1.0.2" % "test",
-        "junit" % "junit" % "4.7" % "test",
         "org.scalaz"   %% "scalaz-core" % "7.0-SNAPSHOT"
       ),
 
       resolvers += "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/",
 
-      testOptions := Seq(
-        Tests.Filter(_ == "qwertybot.BotSpec"),
-        Tests.Argument("html", "console")),
-
-      testOptions <+= crossTarget map { ct =>
-        Tests.Setup { () =>
-          System.setProperty("specs2.outDir", new File(ct, "specs2").getAbsolutePath)
-        }
-      },
-
       botDirectory := file("bots"),
 
       play <<= (botDirectory, name, javaOptions, unmanagedClasspath in Compile, Keys.`package` in Compile) map { (bots, name, javaOptions, ucp, botJar) =>
+
+
         IO createDirectory (bots / name)
         IO copyFile (botJar, bots / name / "ScalatronBot.jar")
 

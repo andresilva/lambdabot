daa6d2019423356f1e4c54c498bee0e54a781be2
Andre Silva <andre.beat@gmail.com>
2012-07-17 15:17:20 +0100

    Updated VM

--- src/main/scala/VM.scala
+++ src/main/scala/VM.scala
@@ -1,15 +1,40 @@
 package lambdabot
 
+import com.sun.jersey.core.util.Base64
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
 import scala.collection.immutable.Stack
 
+import scalaz.NonEmptyList
+
 import VM.Types._
+import sun.misc.BASE64Decoder
 
-case class VM(s: Stack[(Tick, Opcode)]) {
-  def push(t: Tick, o: Opcode): VM = VM(s.push((t, o)))
+case class VM(stack: NonEmptyList[(Tick, Opcode)]) {
+  def push(o: Opcode)(implicit t: Tick): VM = VM((t, o) <:: stack)
 
-  def serialize(t: Tick): String = s.filter(_._1 == t).map(_._2.serialize).mkString("|")
+  def peek = stack.head._2
 
-  def b64serialize: String = "" // TODO
+  def serialize(t: Tick): String = {
+    // TODO lookup for a set instruction and merge maps
+    val newVM = this.push(Set(Map("vm" -> this.b64serialize)))(t)
+    newVM.stack.list.filter(_._1 == t).map(_._2.serialize).mkString("|")
+  }
+
+  def serialize: String = serialize(tick)
+
+  def tick = stack.head._1
+
+  def b64serialize: String = {
+    val baos = new ByteArrayOutputStream
+    val oos = new ObjectOutputStream(baos)
+    oos.writeObject(this)
+    oos.close()
+    new String(Base64.encode(baos.toByteArray))
+  }
 }
 
 object VM {
@@ -17,18 +42,26 @@ object VM {
     type Tick = Int
   }
 
-  def apply(o: Opcode): VM = new VM(Stack((0, o)))
-
   def apply(input: String): Option[VM] = {
     val opcode = Opcode.deserialize(input)
 
     opcode match {
-      case r: React => r.map.get("vm").map(VM.b64deserialize).map(_.push(r.time, r))
-      case w: Welcome => Some(VM.apply(opcode))
+      case r: React => {
+        r.map.get("vm").map(VM.b64deserialize).map { vm =>
+          vm.push(r)(vm.tick)
+        }.orElse {
+          Some(new VM(NonEmptyList((0, r))))
+        }
+      }
       case _ => None
     }
   }
 
-  // deserialize vm from base64string
-  private def b64deserialize(s: String): VM = new VM(Stack[(Tick, Opcode)]())
+  private def b64deserialize(s: String): VM = {
+    val data = new BASE64Decoder().decodeBuffer(s)
+    val ois = new ObjectInputStream(new ByteArrayInputStream(data))
+    val vm  = ois.readObject
+    ois.close()
+    vm.asInstanceOf[VM]
+  }
 }

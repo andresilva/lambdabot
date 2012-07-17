940b4a41521c98cb66a75d4f2704755f0227736f
Andre Silva <andre.beat@gmail.com>
2012-07-17 15:18:24 +0100

    Coordinate and view util methods

--- src/main/scala/Coordinate.scala
+++ src/main/scala/Coordinate.scala
@@ -1,7 +1,16 @@
 package lambdabot
 
+import scala.util.Random
+
 case class Coordinate(x: Int, y: Int) {
   override def toString = "%s:%s".format(x, y)
+
+  def +(that: Coordinate) = Coordinate(x + that.x, y + that.y)
+  def -(that: Coordinate) = Coordinate(x - that.x, y - that.y)
+  def *(factor: Double) = Coordinate((x * factor).toInt, (y * factor).toInt)
+
+  def distance(that: Coordinate) = (this-that).length
+  def length = math.sqrt(x * x + y * y)
 }
 
 object Coordinate {
@@ -14,5 +23,11 @@ object Coordinate {
     } catch {
       case _: MatchError => None
     }
+  }
+
+  def random = Coordinate(Random.nextInt(3) - 1, Random.nextInt(3) - 1)
+
+  object Implicits {
+    implicit def tupleToCoordinate(t: (Int, Int)) = Coordinate(t._1, t._2)
   }
 }
--- src/main/scala/View.scala
+++ src/main/scala/View.scala
@@ -1,6 +1,39 @@
 package lambdabot
 
-case class View(v: IndexedSeq[Cell])
+import scala.math._
+
+case class View(v: IndexedSeq[Cell]) {
+  lazy val relativeView = v.zipWithIndex.map { case (cell, index) => (cell, relativeFromIndex(index)) }
+
+  val size = sqrt(v.length).toInt
+  val center = Coordinate(size / 2, size / 2)
+
+  def apply(c: Coordinate) = v(indexFromRelative(c))
+
+  def myself: Coordinate = center
+
+  def distance(to: Coordinate): Int = {
+    // TODO: take walls into account
+    abs(to.x - myself.x) + abs(to.y - myself.y)
+  }
+
+  def nearest[T]: Option[Coordinate] = {
+    val filtered = relativeView.filter(c => c._1.isInstanceOf[T] && c._2 != myself).map(_._2)
+
+    if (filtered.isEmpty) {
+      None
+    } else {
+      Some(filtered.minBy(distance))
+    }
+  }
+
+  def indexFromRelative(relative: Coordinate) = indexFromAbsolute(absoluteFromRelative(relative))
+  def indexFromAbsolute(absolute: Coordinate) = absolute.x + absolute.y * size
+  def relativeFromIndex(index: Int) = relativeFromAbsolute(absoluteFromIndex(index))
+  def relativeFromAbsolute(abs: Coordinate) = abs - center
+  def absoluteFromIndex(index: Int) = Coordinate(index % size, index / size)
+  def absoluteFromRelative(relative: Coordinate) = relative + center
+}
 
 object View {
   def apply(s: String) = {
